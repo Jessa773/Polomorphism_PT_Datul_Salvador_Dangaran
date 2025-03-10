ac33493cd6fb63a56baf91435a6de353cf02cba1
Jessa773 <jdatul773@gmail.com>
2025-03-10 19:17:36 +0800

    Full-time.php

---
+++ Full-time.php
@@ -0,0 +1,18 @@
+<?php
+class FullTimeEmployee extends Employee implements Bonus {
+    private $monthlySalary;
+
+    public function __construct($name, $id, $position, $monthlySalary) {
+        parent::__construct($name, $id, $position);
+        $this->monthlySalary = $monthlySalary;
+    }
+
+    public function getSalary() {
+        return $this->monthlySalary;
+    }
+
+    public function calculateBonus() {
+        return $this->monthlySalary * 0.10;
+    }
+}
+?>

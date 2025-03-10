819c0b2c5c17421037c0e89e65444440611a0c04
Jessa773 <jdatul773@gmail.com>
2025-03-10 19:19:36 +0800

    Contract.php

---
+++ Contract.php
@@ -0,0 +1,14 @@
+<?php
+class ContractEmployee extends Employee {
+    private $contractAmount;
+
+    public function __construct($name, $id, $position, $contractAmount) {
+        parent::__construct($name, $id, $position);
+        $this->contractAmount = $contractAmount;
+    }
+
+    public function getSalary() {
+        return $this->contractAmount;
+    }
+}
+?>
